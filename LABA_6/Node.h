c2d1ef909b220f0ffbcf86b60f3a206ccf47752b
eropf4 <44525878+eropf4@users.noreply.github.com>
2020-05-25 05:52:59 +0400

    Add files via upload

---
+++ LABA_6/Node.h
@@ -0,0 +1,14 @@
+#pragma once
+#pragma once
+class Node {
+public:
+    Node(int value);
+    ~Node();
+    Node* getNext() const;
+    int getValue() const;
+    void push(Node* node);
+    void deleteNext();
+private:
+    int value;
+    Node* next = nullptr;
+};
