09974be6d58fee90595e34ef4697677a9727e54f
eropf4 <44525878+eropf4@users.noreply.github.com>
2020-05-25 05:51:40 +0400

    Add files via upload

---
+++ LABA_2.cpp
@@ -0,0 +1,48 @@
+﻿// LABA_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream> 
+#include <string> 
+
+using namespace std;
+
+int Max(int* startObject, int* endObject)
+{
+	int max = INT32_MIN;
+	for (int i = 0; i < (endObject - startObject + 1); i++)
+		if (startObject[i] > max) max = startObject[i];
+	return max;
+}
+
+void Sort(int* st, int* end)
+{
+
+	for (int i = 0; i < (end - st + 1); i++)
+	{
+		for (int j = i + 1; j < (end - st + 1); j++)
+		{
+			if (*(st + i) > * (st + j))
+			{
+				int t = *(st + i);
+				*(st + i) = *(st + j);
+				*(st + j) = t;
+			}
+		}
+	}
+}
+
+
+
+
+int main()
+{
+	int arr[] = { 8, 4, 0, 123, 1234, 672, 1 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	Sort(arr, arr + n - 1);
+	cout << "Sort: " << endl ; 
+	for (int i = 0; i < n; i++)
+		cout << *(arr + i) << " ";
+	cout << endl << "Array Maximum: " << endl << Max(arr, arr + n - 1) << endl;
+}
+
+
