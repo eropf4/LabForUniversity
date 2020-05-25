db76fb77d0950cc4f673c936926901d3d0e41ad9
eropf4 <44525878+eropf4@users.noreply.github.com>
2020-05-25 05:52:12 +0400

    Add files via upload

---
+++ LABA_3.cpp
@@ -0,0 +1,59 @@
+﻿// LABA_3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream> 
+#include <string> 
+
+using namespace std;
+
+
+int findMin(int* startObject, int* endObject)
+{
+	int min = INT32_MAX;
+	for (int i = 0; i < (endObject - startObject + 1); i++)
+		if (startObject[i] < min) min = startObject[i];
+	return min;
+}
+
+
+void findElement(int* startObject, int* endObject) 
+{
+	for (int i = 1; i < (endObject - startObject + 1); i++) {
+		if (startObject[i] - startObject[i - 1] != 1) {
+			cout << startObject[i] - 1 << endl;
+			break;
+		}
+	}
+}
+
+int PredMin(int* startObject, int* endObject) 
+{
+	int min = INT32_MAX;
+	int predmin = INT32_MAX;
+	for (int i = 0; i < (endObject - startObject + 1); i++)
+	{
+		if (startObject[i] < min)
+		{
+			if (min < predmin) predmin = min;
+			min = startObject[i];
+		}
+	}
+
+	return predmin;
+}
+
+int main()
+{
+	int arr[] = { 8, 4, 0, 123, 1234, 672, 1};
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	cout << "Minimal: " << findMin(arr, arr + n - 1) << endl;
+	cout << "PredMinimal: " << PredMin(arr, arr + n - 1) << endl;
+
+	int newArr[] = { 1,2,3,5,6,7,8,9 };
+	int m = sizeof(newArr) / sizeof(arr[0]);
+	cout << " Missing element: ";
+	findElement(newArr, newArr + m - 1);
+
+	return 0;
+}
